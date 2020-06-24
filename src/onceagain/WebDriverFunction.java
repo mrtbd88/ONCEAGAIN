e8b03b2612afb259aee41407e54575c8b590ebc4
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-23 23:12:46 -0400

    once again project done

--- src/onceagain/WebDriverFunction.java
+++ src/onceagain/WebDriverFunction.java
@@ -1,11 +1,16 @@
 package onceagain;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WebDriverFunction {
-	WebDriver driver;
+WebDriver driver;
+	
 
 	public void main(String[] args) {
-		// TODO Auto-generated method stub 
-
+		driver=new ChromeDriver();
+		driver.getCurrentUrl();
 	}
 
 }
